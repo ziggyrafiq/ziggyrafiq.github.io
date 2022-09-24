7d7f300536965bef1348908a89adb8db33d293e1
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2022-09-24 17:03:57 +0100

    Update ZR-Scripts.js

--- Scripts/ZR-Scripts.js
+++ Scripts/ZR-Scripts.js
@@ -23,7 +23,7 @@ function GeBlogPosts(data,blogTabCountClicks) {
             var div = document.createElement("div");
             div.classList.add('col-md-4');
             div.classList.add('p-2');
-            div.innerHTML += `<div  class='card'><img src='images/${data[i].BlogPostImage}' class='card-img'>               
+            div.innerHTML += `<div  class='card'><img src='Images/${data[i].BlogPostImage}' class='card-img'>               
         <div class='card-body'><p  class='bold-text '>${data[i].BlogPostTitle}</p><p class=' text-left'>${data[i].BlogPostDescription}</p>       
         <a href='${data[i].BlogPostUrl}' class='btn-default-colour p-2 ' target='_blank'>Click Here To Read This Blog Post</a></div></div></div>`
             demoBlogPostsContainer.appendChild(div);
